a01a42a03fe9bd45230b0964b94273b604839a16
duongndhe140156@fpt.edu.vn <Duong1211200>
2023-02-19 21:36:42 +0700

    re-add be for user management

---
+++ BE/NitStore/NitStore/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NitStore.Data;
+
+namespace NitStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly NitDbContext dbContext;
+        public AdminController(NitDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<IActionResult> HomeAdmin()
+        {
+            return View();
+        }
+        public async Task<IActionResult> ViewAllUser()
+        {
+            var userList = await dbContext.users.ToListAsync();
+            return View(userList);
+        }
+        
+    }
+}
