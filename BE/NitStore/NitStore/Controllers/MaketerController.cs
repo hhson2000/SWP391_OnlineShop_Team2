9db6f9442d1d42f5b40eb070f195c1d1fbfc6ffe
duongndhe140156@fpt.edu.vn <Duong1211200>
2023-02-21 22:48:24 +0700

    update be of maketer

---
+++ BE/NitStore/NitStore/Controllers/MaketerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NitStore.Data;
+
+namespace NitStore.Controllers
+{
+    public class MaketerController : Controller
+    {
+        private readonly NitDbContext dbContext;
+        public MaketerController(NitDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> HomeMaketer()
+        {
+            return View();
+        }
+        public async Task<IActionResult> ViewAllProduct()
+        {
+            return View();
+        }
+    }
+}
