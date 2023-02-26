ac69157a2d6ed3017edf9be52d9603b80eeaae68
LongNT <longnthe140162@fpt.edu.vn>
2023-02-27 05:10:35 +0700

    Image Unit test

---
+++ BE/NitStore/NITStoreTest/ImageTest.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NitStore.Data;
+using NitStore.Models.Domain;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assert = NUnit.Framework.Assert;
+
+namespace NITStoreTest
+{
+    [TestFixture]
+    public class ImageTest
+    {
+        private DbContextOptions<NitDbContext> _options;
+
+        [SetUp]
+        public void Setup()
+        {
+            _options = new DbContextOptionsBuilder<NitDbContext>()
+                .UseInMemoryDatabase(databaseName: "testDatabase")
+                .Options;
+        }
+
+        [Test]
+        public void CreateImage_AddsNewImageToDatabase()
+        {
+            // Arrange
+            using var context = new NitDbContext(_options);
+            var image = new Image
+            {
+                ImageURL = "Test  url",
+                Description = "This is a test image"
+            };
+
+            // Act
+            context.Image.Add(image);
+            context.SaveChanges();
+
+            // Assert
+            var savedImage = context.Image.FirstOrDefault(c => c.Id == image.Id);
+            Assert.NotNull(savedImage);
+            Assert.AreEqual(image.ImageURL, savedImage.ImageURL);
+            Assert.AreEqual(image.Description, savedImage.Description);
+        }
+
+        [Test]
+        public void ReadImage_GetsImageFromDatabase()
+        {
+            // Arrange
+            using var context = new NitDbContext(_options);
+            var image = new Image
+            {
+                ImageURL = "Test  url",
+                Description = "This is a test image"
+            };
+            context.Image.Add(image);
+            context.SaveChanges();
+
+            // Act
+            var savedImage = context.Image.FirstOrDefault(c => c.Id == image.Id);
+
+            // Assert
+            Assert.NotNull(savedImage);
+            Assert.AreEqual(image.ImageURL, savedImage.ImageURL);
+            Assert.AreEqual(image.Description, savedImage.Description);
+        }
+
+        [Test]
+        public void UpdateImage_UpdatesImageInDatabase()
+        {
+            // Arrange
+            using var context = new NitDbContext(_options);
+            var image = new Image
+            {
+                ImageURL = "Test  url",
+                Description = "This is a test image"
+            };
+            context.Image.Add(image);
+            context.SaveChanges();
+
+            // Act
+            var savedImage = context.Image.FirstOrDefault(c => c.Id == image.Id);
+            savedImage.ImageURL = "Test URL";
+            savedImage.Description = "This is an updated test category";
+            context.SaveChanges();
+
+            // Assert
+            var updatedImage = context.Image.FirstOrDefault(c => c.Id == image.Id);
+            Assert.NotNull(updatedImage);
+            Assert.AreEqual(savedImage.ImageURL, updatedImage.ImageURL);
+            Assert.AreEqual(savedImage.Description, updatedImage.Description);
+        }
+
+        [Test]
+        public void DeleteImage_RemovesImageFromDatabase()
+        {
+            // Arrange
+            using var context = new NitDbContext(_options);
+            var image = new Image
+            {
+                ImageURL = "Test  url",
+                Description = "This is a test image"
+            };
+            context.Image.Add(image);
+            context.SaveChanges();
+
+            // Act
+            context.Image.Remove(image);
+            context.SaveChanges();
+
+            // Assert
+            var deletedImage = context.Image.FirstOrDefault(c => c.Id == image.Id);
+            Assert.Null(deletedImage);
+        }
+    }
+}
+
