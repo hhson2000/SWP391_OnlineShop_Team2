609d9e599bed303c59c07f7bb8e9623d431446d8
duongndhe140156@fpt.edu.vn <Duong1211200>
2023-02-28 15:47:04 +0700

    refactor AuthenController

--- BE/NitStore/NitStore/Controllers/AuthenController.cs
+++ BE/NitStore/NitStore/Controllers/AuthenController.cs
@@ -1,10 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NitStore.Data;
 
 namespace NitStore.Controllers
 {
     public class AuthenController : Controller
     {
-        public IActionResult Index()
+        private readonly NitDbContext dbContext;
+        public AuthenController(NitDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register()
         {
             return View();
         }
