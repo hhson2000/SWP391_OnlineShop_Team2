570614ed2b4ce955aaa4a4aa866c4dfe4949d12d
LongNT <longnthe140162@fpt.edu.vn>
2023-03-15 10:31:27 +0700

    Add Login and Register for Guest/Customer

--- BE/NitStore/NitStore/Controllers/AuthenController.cs
+++ BE/NitStore/NitStore/Controllers/AuthenController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NitStore.Data;
 using NitStore.Models.Domain;
 using NitStore.Models.DTO;
@@ -19,6 +20,29 @@ namespace NitStore.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Login(User loginUser)
+        {
+            if (loginUser != null)
+            {
+                string username = loginUser.UserName;
+                string password = loginUser.Password;
+                var user = await dbContext.users.Where(u => u.UserName.Equals(username) && u.Password.Equals(password)).FirstAsync();
+                if(user != null)
+                {
+                    if(user.Role == 5)
+                    {
+                        return RedirectToAction("ListProduct", "Products", new { area = "" });
+                    }
+                    
+                }
+            } else
+            {
+                return View();
+            }
+            return View();
+
+        }
 
         [HttpGet]
         public async Task<IActionResult> Register()
@@ -29,15 +53,47 @@ namespace NitStore.Controllers
         [HttpPost]
         public async Task<IActionResult> Register(RegisterUserDTO user)
         {
-            if (user == null)
+            if (user != null)
             {
-
+                if (user.Password.Equals(user.RePassword)) {
+                    User newUser = new User
+                    {
+                        UserName = user.UserName,
+                        Email = user.Email,
+                        Password = user.Password,
+                        Role = 5,
+                        Status = 1
+                    };
+                    try
+                    {
+                        dbContext.users.Add(newUser);
+                        await dbContext.SaveChangesAsync();
+                        UserDetail userDetail = new UserDetail
+                        {
+                            Id = newUser.Id,
+                            Name = user.Name,
+                            Address = user.Address,
+                            PhoneNumber = user.PhoneNumber,
+                            Gender = user.Gender
+                        };
+                        dbContext.userDetail.Add(userDetail);
+                        await dbContext.SaveChangesAsync();
+                        return RedirectToAction("Login");
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                        return View();
+                    }
+                } else
+                {
+                    return View();
+                }
             }
             else
             {
-
+                return View();
             }
-            return View();
         }
     }
 }
